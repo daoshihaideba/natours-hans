82ee6f5581d41bf6541290720da0796b52201777
hans.zhang <hans.zhang@sap.com>
2023-03-21 16:18:47 +0800

    implemented cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
-
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -24,6 +24,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+
+// Implement cors
+app.use(cors());
+// Access-Controll-Allow-Origin
+// api.natours.com, front-end natours.com
+
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com'
+//   })
+// );
+app.options('*', cors());
+// app.options('/api/v1/tours/:id',cors())
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
